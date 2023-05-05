c9b1d622e9f1732189fac9a3841eed84cd704ad3
nasiru hammed aimanosi <nasaces360@gmail.com>
2023-05-05 23:19:03 +0100

    add client side to the graphql-server

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+// components
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Nasiru's reading list</h1>
+          <BookList />
+          <AddBook />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
